b2cb646a0d4bd34ab23cac6e0c2af7de76d7de30
Tomi Adenekan <tominekan12@gmail.com>
2025-06-07 20:25:13 -0400

    Actually updated readme

--- release.sh
+++ release.sh
@@ -2,3 +2,10 @@
 python manage.py migrate
 
 # Make use of the custom bootstrap
+mkdir custom_bootstrap
+cd custom_bootstrap
+npm init -y
+npm install bootstrap@5.3.6
+
+npm install -g sass
+sass ./scss/custom.scss ./css/custom.css
