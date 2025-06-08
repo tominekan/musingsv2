0e82225ab7176dd997b30a49c556c1c16872b747
Tomi Adenekan <tominekan12@gmail.com>
2025-06-08 13:54:37 -0400

    Updating settings.py to get build to work

--- musingsv2/settings.py
+++ musingsv2/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["musings-xcu4.onrender.com"]
 
 
 # Application definition
