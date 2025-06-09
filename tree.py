f1b12118f792bc3fcc650b6189f68788137a4ff3
Tomi Adenekan <tominekan12@gmail.com>
2025-06-09 12:58:19 -0400

    Updated tree.py to be more clear

--- tree.py
+++ tree.py
@@ -1,7 +1,17 @@
 import os
 
-for (root,dirs,files) in os.walk('.', topdown=True):
-        print (root)
-        print (dirs)
-        print (files)
-        print ('--------------------------------')
+for root, subdir, files in os.walk("."):
+    print(f"Inside the {'root' if root == '.' else root} directory")
+    print(f"{'' if len(subdir) == 0 else subdir}")
+    
+    for file in files:
+        print(f"    - {file}")
+    print("————————————————————————————————————————————————————————————————————————————————————————")
+    if ".git" in subdir:
+        subdir.remove(".git")
+    if "__pycache__" in subdir:
+        subdir.remove("__pycache__")
+    if "node_modules" in subdir:
+        subdir.remove("node_modules")
+    if ".venv" in subdir:
+        subdir.remove(".venv")
