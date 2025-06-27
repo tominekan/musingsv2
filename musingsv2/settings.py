d947cc69f5d303f52fef4c2f981a717c5c389f87
Tomi Adenekan <tominekan12@gmail.com>
2025-06-26 21:50:27 -0400

    Desperately trying to fix this lol

--- musingsv2/settings.py
+++ musingsv2/settings.py
@@ -139,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Add Media (specifically article images)
 MEDIA_ROOT = "musingsv2/media"
